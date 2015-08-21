f48106070a31222c7ad11d589eeb8f7c3d427f34
Fraser <frasermince@gmail.com>
2015-08-21 15:56:23 -0500

    Populate original values of env.

---
+++ src/Evaluator.hs
@@ -0,0 +1,29 @@
+module Evaluator
+() where
+
+import qualified Data.Map as Map
+import Types               (Sexp(..))
+import Control.Applicative (liftA2)
+
+
+add :: Sexp -> Sexp -> Sexp
+add (MalNum x) (MalNum y) = MalNum (x + y)
+
+subtract :: Sexp -> Sexp -> Sexp
+subtract (MalNum x) (MalNum y) = MalNum (x - y)
+
+multiply :: Sexp -> Sexp -> Sexp
+multiply (MalNum x) (MalNum y) = MalNum (x * y)
+
+divide :: Sexp -> Sexp -> Sexp
+divide (MalNum x) (MalNum y) = MalNum (x `div` y)
+
+--replEnv :: [Map.Map String Sexp]
+replEnv = [Map.fromList [("+", add), ("-", Evaluator.subtract), ("*", multiply), ("/", divide)]]
+
+type RunTimeError = String
+
+evaluate :: Sexp -> Either RunTimeError Sexp
+evaluate = Right
+
+
