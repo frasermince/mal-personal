618e47853ee875fd2bd7332de711d9e4b3dbf99a
Fraser Mince <frasermince@gmail.com>
2015-09-07 12:14:48 -0500

    Add haskeline.
    We now have history and backspace support.

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,16 @@ import Control.Monad
 import System.IO
 import Rep
 import Environment(replEnv)
+import System.Console.Haskeline
 
 main :: IO ()
-main = forever prompt
-
-prompt :: IO ()
-prompt = do
-  putStr "user> "
-  hFlush stdout
-  command <- getLine
-  putStrLn $ either show show $ rep replEnv command
-
+main = runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "user> "
+           case minput of
+               Nothing -> return ()
+               Just "quit" -> return ()
+               Just command -> do outputStrLn $ either show show $ rep replEnv command
+                                  loop
