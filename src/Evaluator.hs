80aa1260b22ebd291fff3e82450db15fa25672b4
Fraser Mince <frasermince@gmail.com>
2015-09-21 17:46:23 -0500

    Add let binding to evaluate.

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,13 +1,27 @@
 module Evaluator
 (evaluate) where
 
-import Types                        (Sexp(..), Environment(..), RunTimeError(..), Eval(..), runEval, MalError(..))
+import Types                        (Sexp(..), Environment(..), RunTimeError(..), Eval(..), runEval, MalError(..), runEvalForTuple)
 import qualified Environment as Env (get, set)
 import Control.Monad.Writer.Lazy
 import Control.Monad.Except
 
 
 evaluate :: (Sexp, Environment) -> Eval
+evaluate (MalList (MalSymbol "let*" : MalList params : expression : []), env) = do (_, newEnv) <- listen $ findNewEnv params
+                                                                                   evaluate (expression, newEnv)
+  where findNewEnv :: [Sexp] -> Eval
+        --need to make add and remove layer commands for env
+        findNewEnv [] = censor (\x -> env) (return $ MalList [])
+
+        findNewEnv (_:[]) = do throwError $ MalEvalError "let* param bindings do not match"
+        findNewEnv (MalSymbol key : val : params) = do unwrappedVal <- solvedVal
+                                                       censor (Env.set key unwrappedVal) evalledCurrentEnv
+          where evalledCurrentEnv = findNewEnv params
+                solvedVal =  do (_, currentEnv) <- listen evalledCurrentEnv
+                                evalAst (val, currentEnv)
+        --findNewEnv _ = do throwError $ MalEvalError "Wrong number of params to let binding"
+
 evaluate (MalList (MalSymbol "def!" : MalSymbol key : value : []), env) = do solvedValue <- evalAst (value, env)
                                                                              tell $ Env.set key solvedValue env
                                                                              return solvedValue
